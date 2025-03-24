63635146a8fbd82fa8ac5e063db6c2005a8829e6
Mamytina <haingotianatana7@gmail.com>
2025-03-24 19:02:35 +0300

    Description des modifications users

--- superAdmin/models.py
+++ superAdmin/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class superAdmin(models.Model):
+class SuperAdmin(models.Model):
     name = models.CharField(max_length=100)  
     password = models.CharField(max_length=255)  
 
--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
-class Users():
+class Users(models.Model):
     first_name = models.CharField(max_length=50) 
     last_name = models.CharField(max_length=50)   
     email = models.EmailField(unique=True)       
